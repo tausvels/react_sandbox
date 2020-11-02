db7495f0d1914e08220fc0ec9c18f567cd6033d8
Tausif Ezaj Khan <49699301+tausvels@users.noreply.github.com>
2020-11-01 20:41:07 -0800

    initial commit

---
+++ boilerplate/src/screen/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  console.log("I am Dashboard");
+
+  return (
+    <div className="ttDashboard">
+      <h1>I am Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
